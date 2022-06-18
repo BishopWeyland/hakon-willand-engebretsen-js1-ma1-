d36167f072e79e64c2da4b375cbfd5c8308604c3
Håkon Willand Engebretsen <willand27@hotmail.com>
2022-06-18 13:40:01 +0200

    Question 5

--- js/script.js
+++ js/script.js
@@ -35,3 +35,13 @@ heading.style.fontSize = "2em";
 // Question 4
 
 heading.className = "subheading";
+
+// Question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i++) {
+  paragraphs[i].style.color = "red";
+}
+
+// Question 6
